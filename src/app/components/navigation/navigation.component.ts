602c826de5970ad9c78cdd1005c05641132fc38a
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2024-12-01 08:09:17 +0100

    add selected route style to navigation

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { AuthenticationService } from "./../../services/authentication.service";
 import { Component } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
@@ -8,9 +9,11 @@ import { Router } from "@angular/router";
   templateUrl: "./navigation.component.html",
   styleUrls: ["./navigation.component.css"],
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, CommonModule],
 })
 export class NavigationComponent {
+  selectedRoute: string = '';
+
   constructor(
     private authentication: AuthenticationService,
     private router: Router
@@ -21,6 +24,7 @@ export class NavigationComponent {
   }
 
   public navigateTo(route: string): void {
+    this.selectedRoute = route;
     this.router.navigate([`/main/${route}`]);
   }
 }
